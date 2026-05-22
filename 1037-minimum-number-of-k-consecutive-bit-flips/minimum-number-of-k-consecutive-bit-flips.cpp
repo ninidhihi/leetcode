0a04d5a4b9fc105f8177869ea9a2abb8728fdb46
Nidhi Kumari <nidhishital11@gmail.com>
2026-05-22 23:37:06 +0530

    Time: 0 ms (100.00%) | Memory: 110 MB (95.94%) - LeetSync

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int flips = 0;
+        
+        int flipCountFromPastForCurri = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(i >= k && nums[i-k] == 2) { //Was it flipped
+                flipCountFromPastForCurri--;
+            }
+            
+            if(flipCountFromPastForCurri % 2 == nums[i]) {
+                if(i+k > n){
+                    return -1;
+                }
+                flipCountFromPastForCurri++;
+                nums[i] = 2; //Marking as flipped
+                flips++;
+            }
+        }
+
+        return flips;
+    }
+};
