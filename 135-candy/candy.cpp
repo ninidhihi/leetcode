129937bcc11c45f4bb916e52501ad4d16705c795
Nidhi Kumari <nidhishital11@gmail.com>
2026-06-24 12:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (68.60%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> randi(n, 1);
+        vector<int> rai(n, 1);
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                randi[i] = randi[i - 1] + 1;
+                rai[i] = rai[i - 1] + 1;
             }
         }
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-                randi[i] = max(randi[i], randi[i + 1] + 1);
+                rai[i] = max(rai[i], rai[i + 1] + 1);
             }
         }
 
         int total = 0;
-        for (int c : randi)
+        for (int c : rai)
             total += c;
 
         return total;
